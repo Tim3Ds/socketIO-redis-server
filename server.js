df4137fd4d2b57e9915ad2aaf570a16acc54e8b6
Tim <Tim@kellydev.net>
2020-05-06 08:55:46 -0600

    init

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const { Kafka } = require('kafkajs');
+
+const { piece } = require('./spec.js');
+
+const kafka = new Kafka({
+  clientId: 'chess-app',
+  brokers: ['192.168.1.137:9092', 'localhost:9092']
+})
+ 
+const producer = kafka.producer();
+producer.connect();
+
+const consumer = kafka.consumer({ groupId: 'chess-server' });
+consumer.connect()
+consumer.subscribe({ topic: 'board' })
+consumer.subscribe({ topic: 'move' })
+
+io.on('connection', async (socket) => {
+	console.log('Setting up comunications');
+	
+	socket.on('disconnect', function(){
+	  console.log('user disconnected');
+	});
+	
+	socket.on('clean-board', () => {
+		console.log('call clean board');
+	  	socket.emit('clean-square', 'all');    
+	});
+
+	socket.on('set-board-to-state', boardState => {
+		// console.log('set-board-to-state', boardState);
+		for ( let color in boardState ){
+			// console.log(color)
+			for ( let pieceType in boardState[color] ){
+				// console.log(boardState[color][pieceType])
+				for ( let id in boardState[color][pieceType] ){
+					// console.log( color, pieceType, piece[color][pieceType])
+					socket.emit('set-piece-on-square', {
+						"id": boardState[color][pieceType][id],
+						"color": color,
+						"type": pieceType,
+						"display": piece[color][pieceType]
+					});
+				}
+			}
+		}
+	});
+	
+	socket.on('place-piece', item => {
+		console.log('place-piece' + item);
+		socket.emit('set-piece-on-square', item);
+	});
+
+	socket.on('square-clicked', item => {
+		// console.log(item);
+		socket.emit('clean-square', 'highlight');
+		if (item.type != null){
+			// call Kafka send topic=type data=location
+			console.log('item type null');
+			producer.connect();
+			producer.send({
+				"topic": 'board',
+				"messages": [
+				  { "key": item.type , "value": JSON.stringify(item) },
+				],
+			})
+		}
+	});
+	await consumer.run({
+		eachMessage: async ({ topic, message }) => {
+			let value = JSON.parse(message.value.toString());
+			let key = message.key.toString();
+			console.log("consumer \n", topic, key, value);
+			
+			if (key == 'highlite'){
+				for (let potentialMove in value.moveOptions){
+					socket.emit('highlite-square', {
+						id: value.moveOptions[potentialMove], 
+						player: value.player 
+					});
+				}
+			}
+		
+		}
+	})
+
+	console.log('user connected comunications configured');
+});
+
+http.listen(9591, () => {
+	console.log('socket server on port 9591');
+});
+
---
+++ spec.js
@@ -0,0 +1,19 @@
+module.exports.piece = {
+    white: {
+        King: '\u2654',
+        Queen: '\u2655',
+        Rook: '\u2656',
+        Bishop: '\u2657',
+        Knight: '\u2658',
+        Pawn: '\u2659'
+    },
+    black: {
+        King: '\u265A',
+        Queen: '\u265B',
+        Rook: '\u265C',
+        Bishop: '\u265D',
+        Knight: '\u265E',
+        Pawn: '\u265F'
+    }
+};
+
