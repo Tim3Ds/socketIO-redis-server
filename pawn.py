2ff3819d396ebe1b524c4dfc57c86c4baa4b0d78
Tim3Ds <Tim3Ds@gmail.com>
2020-05-14 09:31:15 -0600

    add bits and peces

--- pawn.py
+++ pawn.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 
 import json
-from kafka import KafkaConsumer, KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['192.168.1.3:9092'])
-# producer.send('test', b'Hello, World!')
-# producer.send('test', key=b'message-two', value=b'This is Kafka-Python')
+letterToNum = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+}
+# catch item
+item = {
+    "type": "Pawn",
+    "player": "black",
+    "location": {
+        "x": "a",
+        "y": "2"
+    },
+}
 
-consumer = KafkaConsumer('test', 'board', 'moves',
-                         bootstrap_servers=['192.168.1.3:9092'], group_id='chess-piece-pawn'
-                        )
+if item['type'] == 'Pawn':
+    x = letterToNum[item['location']['x']]
+    y = int(item['location']['y'])
+    pawnMoves = []
 
-print("test consumer pre message")
-for message in consumer:
-    print(message)
-    key = message.key
-    messageJSON = json.loads(message.value)
-    print(key, messageJSON['id'])
-    if key == 'Pawn':
-        x = messageJSON['location']['x']
-        y = int(messageJSON['location']['y'])
-        pawnMoves = []
+    if item['player'] == 'black' and y == 2:
+        pawnMoves = [f"{x}{y + 1}", f"{x}{y + 2}" ]
+    else:
+        pawnMoves = [f"{x}{y - 1}", f"{x}{y - 2}" ]
 
-        if messageJSON['player'] == 'black':
-            pawnMoves = [f"{x}{y + 1}", f"{x}{y + 2}" ]
-        else:
-            pawnMoves = [f"{x}{y - 1}", f"{x}{y - 2}" ]
-        
-        producer.send('board', key=b'highlight', value=json.dumps(pawnMoves).encode())
+# send move options
