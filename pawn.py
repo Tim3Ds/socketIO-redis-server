2278bb68c7cb51dd9cee1164a8271ce661c43956
Tim3Ds <Tim3Ds@gmail.com>
2020-05-20 13:15:01 -0600

    use handler callback instead of loop
    also make it more function/reusable

--- pawn.py
+++ pawn.py
@@ -1,59 +1,83 @@
 #!/usr/bin/python3
 
-import json
-import redis
+from json import dumps, loads
+from redis import Redis
+from signal import SIGINT, SIGTERM, signal#, SIGUSR1
+from sys import exit as sysexit
 
-client = redis.Redis(host='192.168.1.3', port=6379, db=0)
+client = Redis(host='192.168.1.3', port=6379, db=0, decode_responses=True)
 
 # print("test redis pre message")
-pubsub = client.pubsub()
-pubsub.subscribe('Pawn')
+pubsub = client.pubsub(ignore_subscribe_messages=True)
 
-xIndex = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+PIECE_TYPE = 'Pawn'
+COL_LETTER_TO_INDEX = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-while True:
-    message = pubsub.get_message()
-    if message:
-        # print(message)
-        print(message['data'])
-        # 1 is the default data responce to subcribing
-        if message['data'] != 1:
-            messageJSON = json.loads(message['data'])
-            if b"Pawn" in message['data']:
-                print(messageJSON['id'])
-                x = xIndex.index(messageJSON['location']['x'])
-                y = int(messageJSON['location']['y'])
-                pawnMoves = []
+def getMoves(messageData):
+	print(messageData['id'])
+	try:
+		col = COL_LETTER_TO_INDEX.index(messageData['location']['x'])
+		row = int(messageData['location']['y'])
+	catch:
+		return
+	
+	pieceMoves = []
 
-                if messageJSON['player'] == 'black':
-                    if y == 2:
-                        pawnMoves = [f"{xIndex[x]}{y + 1}", f"{xIndex[x]}{y + 2}"]
-                    else:
-                        pawnMoves = [f"{xIndex[x]}{y + 1}"]
-                elif messageJSON['player'] == 'white':
-                    if y == 7:
-                        pawnMoves = [f"{xIndex[x]}{y - 1}", f"{xIndex[x]}{y - 2}"]
-                    else:
-                        pawnMoves = [f"{xIndex[x]}{y - 1}"]
+	if messageData['player'] == 'black': #~PJ consider making this an enum.
+		pieceMoves.append(f"{COL_LETTER_TO_INDEX[col]}{row + 1}")
+		if row == 2:
+			pieceMoves.append(f"{COL_LETTER_TO_INDEX[col]}{row + 2}")
 
-                for move in pawnMoves:
-                    print(move)
-                    responce = {
-                        "type": "highlight",
-                        "gameCode": messageJSON['code'],
-                        "player": messageJSON['player'],
-                        "id": move,
-                        "optionOwner": messageJSON['id']
-                    }
-                    client.publish('board', json.dumps(responce))
+	elif messageData['player'] == 'white':
+		pieceMoves.append(f"{COL_LETTER_TO_INDEX[col]}{row - 1}")
+		if row == 7:
+			pieceMoves.append(f"{COL_LETTER_TO_INDEX[col]}{row - 2}")
 
-            elif b"move" in message['data']:
-                print(message['data'])
-                responce = {
-                    "moveTo": messageJSON['newID'],
-                    "moveFrom": messageJSON['oldID'],
-                    "player": messageJSON['player'],
-                    "type": 'Pawn',
-                    "gameCode": messageJSON['code'],
-                }
-                client.publish('moves', json.dumps(responce))
+	for moveOption in pieceMoves:
+		print(moveOption)
+		client.publish('board', dumps({
+			"type": "highlight",
+			"gameCode": messageData['code'],
+			"player": messageData['player'],
+			"id": moveOption,
+			"optionOwner": messageData['id']
+		}))
+
+def movePiece(messageData):
+	# print(message['data'])
+	client.publish('moves', dumps({
+		"moveTo": messageData['newID'],
+		"moveFrom": messageData['oldID'],
+		"player": messageData['player'],
+		"type": PIECE_TYPE,
+		"gameCode": messageData['code'],
+	}))
+
+def messageHandler(message):
+	# print(message)
+	#print(message['data'])
+	try:
+		jsonData = loads(message['data'])
+	catch:
+		return
+	
+	if PIECE_TYPE == jsonData['type']:
+		getMoves(jsonData)
+	elif 'move' == jsonData['type']:
+		movePiece(jsonData)
+	else: pass
+
+pubsub.subscribe(**{PIECE_TYPE: messageHandler})
+thread = pubsub.run_in_thread(sleep_time=1)
+
+def signalHandler(signum, frame):
+	thread.stop()
+	thread.join()
+	pubsub.close()
+	sysexit(0)
+
+signal(SIGINT, signalHandler)
+signal(SIGTERM, signalHandler)
+#signal(SIGUSR1, signalHandler)
+
+while True: pass
